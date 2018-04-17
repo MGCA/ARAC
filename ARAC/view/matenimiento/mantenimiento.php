b0be5eb0d209824ac05e76be8a492a2c1f372543
MGCA <maikel.chavarriaalvarado@gmail.com>
2018-04-17 15:48:50 -0600

    Proyecto Inicial
    Madre

---
+++ ARAC/view/matenimiento/mantenimiento.php
@@ -0,0 +1,60 @@
+<link href="../../archivos/css/bootstrap.min.css" rel="stylesheet">
+<link href="../../archivos/css/estilos.css" rel="stylesheet">
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="text-center">Mantenimiento del Sistema</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            Instrucciones
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        <br>
+                        <p>
+                            En esta area podremos gestionar la configuracion
+                            del sistema ARAC,se recomienda que su uso sea 
+                            con cautela y responsable ya que los cambios realizados
+                            podria causar daños a terceros. Para mas informacion
+                            contacte a soporte tecnico del sistema.     
+                        </p>
+                        <br>
+                        <a href="mantenimientoUsuario/nuevoUsuario.php">Ya tienes un Usuario?</a>
+                        <br>
+                        <br>
+                        <a href="#">Olvidaste tu contraseña?</a>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            Gestion de Mantenimiento
+                        </h3>
+
+                    </div>
+                    <div class="panel-body">
+                        <ul class="nav-divider">
+                            <li><a href="/plantillaRegistro/encabezadoPrevista.php">Acueducto</a></li>
+                            <br>
+                            <li><a href="../ARAC/plantillaRegistro/encabezadoEmpleado.php">Empleados</a></li>
+                            <br>
+                            <li><a href="/plantillaRegistro/encabezadoSocio.php">Socios</a></li>
+                            <br>
+                            <li><a href="/plantillaRegistro/encabezadoInventario.php">Inventario</a></li>
+                            <br>
+                            <li><a href="/plantillaRegistro/encabezadoCompra.php">Compras</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="../../archivos/js/jquery.min.js"></script>
+<script src="../../archivos/js/bootstrap.min.js"></script>
---
+++ ARAC/view/presentacion/tramites.php
@@ -0,0 +1,15 @@
+<link href="../../archivos/css/bootstrap.min.css" rel="stylesheet">
+<link href="../../archivos/css/estilos.css" rel="stylesheet">
+<div class="container">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            Tramites
+        </h3>
+
+    </div>
+    <div class="panel-body">
+
+    </div>
+</div>
+<script src="../../archivos/js/jquery.min.js"></script>
+<script src="../../archivos/js/bootstrap.min.js"></script>
