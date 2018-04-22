ee0a6c63f162864933f697df2e9b3c2d71f3a29a
Kimberly <yesse712@gmail.com>
2018-04-21 22:29:41 -0600

    Modificaciones en modelos de Compra y empleado

--- ARAC/entidades/CompraModel.php
+++ ARAC/entidades/CompraModel.php
@@ -1,8 +1,130 @@
 <?php
+class Compra{
+    private $pdo;
+    
+    public $numCompra;
+    public $encargadoCompra;
+    public $nombreNegocio;
+    public $motivoCompra;
+    public $lugarCompra;
+    public $fechaCompra;
+    public $montoTotalCompra;
+    
+    public function __CONSTRUCT()
+	{
+		try
+		{
+			$this->pdo = Database::StartUp();     
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+	public function Listar()
+	{
+		try
+		{
+			$result = array();
 
+			$stm = $this->pdo->prepare("SELECT * FROM compra");
+			$stm->execute();
+
+			return $stm->fetchAll(PDO::FETCH_OBJ);
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Obtener($id)
+	{
+		try 
+		{
+			$stm = $this->pdo
+			          ->prepare("SELECT * FROM compra WHERE numCompra = ?");
+			          
+
+			$stm->execute(array($id));
+			return $stm->fetch(PDO::FETCH_OBJ);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Eliminar($id)
+	{
+		try 
+		{
+			$stm = $this->pdo
+			            ->prepare("DELETE FROM compra WHERE numCompra = ?");			          
+
+			$stm->execute(array($id));
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Actualizar($data)
+	{
+		try 
+		{
+			$sql = "UPDATE compra SET 
+						encargadoCompra          = ?, 
+						nombreNegocio        = ?,
+                                                motivoCompra          = ?,
+						lugarCompra            = ?, 
+						fechaCompra = ?,
+                                                montoTotalCompra = ?
+				    WHERE numCompra = ?";
+                        
+//   numCompra-encargadoCompra-nombreNegocio-motivoCompra-lugarCompra-fechaCompra-montoTotalCompra;
+
+			$this->pdo->prepare($sql)
+			     ->execute(
+				    array(
+                                        $data->encargadoCompra, 
+                                        $data->nombreNegocio,
+                                        $data->motivoCompra,
+                                        $data->lugarCompra,
+                                        $data->fechaCompra,
+                                        $data->montoTotalCompra,
+                                        $data->id
+                                    )
+				);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Registrar(Compra $data)
+	{
+		try 
+		{
+                    //   numCompra-encargadoCompra-nombreNegocio-motivoCompra-lugarCompra-fechaCompra-montoTotalCompra;
+		$sql = "INSERT INTO compra (numCompra,encargadoCompra,nombreNegocio,motivoCompra,lugarCompra,fechaCompra,montoTotalCompra) 
+		        VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+		$this->pdo->prepare($sql)
+		     ->execute(
+				array(
+                                    $data->numCompra,
+                                    $data->encargadoCompra,
+                                    $data->nombreNegocio, 
+                                    $data->motivoCompra, 
+                                    $data->lugarCompra,
+                                    $data->fechaCompra,
+                                    $data->montoTotalCompra
+                                )
+			);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+}
--- ARAC/entidades/EmpleadoModel.php
+++ ARAC/entidades/EmpleadoModel.php
@@ -1,8 +1,134 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Empleado{
+    private $pdo;
+    
+    public $cedula;
+    public $nombre;
+    public $primerApellido;
+    public $segundoApellido;
+    public $direccion;
+    public $telefono;
+    public $puesto;
+    
+    
+    public function __CONSTRUCT()
+	{
+		try
+		{
+			$this->pdo = Database::StartUp();     
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
 
+	public function Listar()
+	{
+		try
+		{
+			$result = array();
+
+			$stm = $this->pdo->prepare("SELECT * FROM empleado");
+			$stm->execute();
+
+			return $stm->fetchAll(PDO::FETCH_OBJ);
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Obtener($id)
+	{
+		try 
+		{
+			$stm = $this->pdo
+			          ->prepare("SELECT * FROM empleado WHERE cedula = ?");
+			          
+
+			$stm->execute(array($id));
+			return $stm->fetch(PDO::FETCH_OBJ);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Eliminar($id)
+	{
+		try 
+		{
+			$stm = $this->pdo
+			            ->prepare("DELETE FROM empleado WHERE cedula = ?");			          
+
+			$stm->execute(array($id));
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Actualizar($data)
+	{
+		try 
+		{
+			$sql = "UPDATE alumnos SET 
+						nombre          = ?, 
+						primerApellido        = ?,
+                                                segundoApellido          = ?,
+						direccion            = ?, 
+						telefono = ?,
+                                                puesto = ?
+				    WHERE cedula = ?";
+                        
+//cedula- nombre- primerApellido- segundoApellido- direccion- telefono- puesto;
+    
+
+			$this->pdo->prepare($sql)
+			     ->execute(
+				    array(
+                                        $data->nombre, 
+                                        $data->primerApellido,
+                                        $data->segundoApellido,
+                                        $data->direccion,
+                                        $data->telefono,
+                                        $data->puesto,
+                                        $data->cedula
+                                    )
+				);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Registrar(Empleado $data)
+	{
+		try 
+		{
+                    //cedula- nombre- primerApellido- segundoApellido- direccion- telefono- puesto;
+		$sql = "INSERT INTO empleado (cedula,nombre,primerApellido,segundoApellido,direccion,telefono,puesto) 
+		        VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+		$this->pdo->prepare($sql)
+		     ->execute(
+				array(
+                                    $data->cedula,
+                                    $data->nombre,
+                                    $data->primerApellido, 
+                                    $data->segundoApellido, 
+                                    $data->direccion,
+                                    $data->telefono,
+                                    $data->puesto,
+                                
+                                )
+			);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+}
